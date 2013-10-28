873922148d54107479df08ceccbeeaafb798a408
Þórður Hermannsson <thordurguhe@gmail.com>
2013-10-28 11:42:13 +0000

    Integration with fpComplete

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,11 @@
 -- | Main entry point to the application.
 module Main where
 
+import           Visual
+
 -- | The main entry point.
 main :: IO ()
 main = do
-    putStrLn "Welcome to FP Haskell Center!"
+    let lis = [201..220 :: Integer]
+    representL $ showWithNrOfBitsL 8 $ showAtBaseL 2 lis
+    putStrLn "Done"
