e616248a17c1fcec81fa165cc9bc49424470909e
Thordur Hermannsson <thordurguhe@gmail.com>
2014-01-01 10:41:30 +0000

    Some useful updates...

--- src/Visual.hs
+++ src/Visual.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Visual where
 
 
@@ -7,11 +9,56 @@ import Data.Bits
 
 {-	Visual 	-}
 
+-- | ByteSize determines how many visual bits
+--   used to represent an Integer
+data ByteSize  = Bit8  | Bit16  | Bit32      deriving Show
+
+-- | Base ---------------------------
+data Base      = Binary | 
+                 Dec    | 
+                 Hex    | 
+                 Oct    
+                 deriving Show
+-------------------------------------
+
+-- | ShowBit. data type for visual representation
+--   from converting between bases
+-- * The actual number to represent
+-- * Maybe ByteSize - if we're showing bits
+-- * base
+data ShowBit   = ShowBit {
+                   nr  :: Integer 
+                  ,btSz :: (Maybe ByteSize)
+                  ,base :: Base
+                 }
+                 deriving Show
+
+
+getBase :: Base -> Integer
+getBase Binary = 2
+getBase Oct    = 8
+getBase Dec    = 10
+getBase Hex    = 16
+
+-- The default ShowBit value
+-- Takes an Integer x and converts to binary
+-- Shows with 8 bits representation
+defShowBit :: Integer -> ShowBit
+defShowBit x = ShowBit x (Just Bit8) Binary
+---------------------------------------
+
+-- bitsIO, gets a value to represent
+-- and a showBit (Example: bitsIO 1 Bit8 Dec >>> [00000001])
+bitsIO :: ShowBit -> IO ()
+bitsIO sb = represent (getBase (base sb)) (nr sb)
+
 represent :: Integer -> Integer -> IO ()
 represent b str = (putStr . format) xstr
 	where format x = "[ " ++ x ++ " ]\n"
               xstr = showSingleAtBase b str
 
+-- base - list -> result IO()
+-- for the moment, call replist
 representL :: Integer -> [Integer] -> IO () 
 representL b xs = mapM_ (putStr . format) xlis
 	where format x = "[ " ++ x ++ " ]\n"
@@ -48,3 +95,4 @@ showWithNrOfBitsL n xs = map (\x -> fill (sd x n)  ++ x) xid
 -- element (if any) to map against
 bitL :: Bits a => (t -> a -> b) -> [a] -> t -> [b]
 bitL f xs d =  map((f) d) xs
+
